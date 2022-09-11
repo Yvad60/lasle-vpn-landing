f2c6d77ff24b6795275ad203cf038156b0da5df6
ivad <yvesyvad@gmail.com>
2022-09-11 14:08:55 +0200

    Add section 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,18 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    colors: {
-      primary_red: '#F53838',
-      primary_black: '#0B132A',
-      secondary_black: '#4F5665',
-      secondary_red: '#FFECEC',
-      primary_gray: '#EEEFF2',
-    },
     extend: {
       screens: {
         '3xl': '1900px',
+      },
+      colors: {
+        primary_red: '#F53838',
+        primary_black: '#0B132A',
+        secondary_black: '#4F5665',
+        secondary_red: '#FFECEC',
+        primary_gray: '#EEEFF2',
+        primary_green: '#2FAB73',
+        primary_bg_gray: '#fbfbfb',
       },
     },
   },
